a1671322f6f9839f0e476f028be31199e4b8dcd3
Timothée R <timo.rebel@yahoo.fr>
2021-12-02 17:50:04 +0100

    Ajout du projet

---
+++ src/test/java/com/example/exercicemongo/ExerciceMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercicemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciceMongoApplicationTests {
+
+    //@Test
+    void contextLoads() {
+    }
+
+}
